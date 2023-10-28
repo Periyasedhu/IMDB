90a395ca5cb82d28494264b8355e671d844a2b27
example-git-student <100669349+Periyasedhu@users.noreply.github.com>
2023-10-28 11:28:15 +0530

    updated key

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 function useFetch(apiPath, queryTerm = "") {
   const [data, setData] = useState([]);
-  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`;
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=25223183a2a809d888eead94a8dba3a2&query=${queryTerm}`;
 
   useEffect(() => {
     async function fetchMovies() {
